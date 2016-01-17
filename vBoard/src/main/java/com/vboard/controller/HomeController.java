0cfdb3dc149954874facd7889aeff4e23bdcd848
A. Gary Bardet <agbardet@gmail.com>
2016-01-17 02:05:22 -0500

    edit board

--- vBoard/src/main/java/com/vboard/controller/HomeController.java
+++ vBoard/src/main/java/com/vboard/controller/HomeController.java
@@ -3,18 +3,22 @@ package com.vboard.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class HomeController {
 	private static final Logger LOGGER = LoggerFactory.getLogger(HomeController.class);
-	
-	@RequestMapping(value="/", method= RequestMethod.GET)
-	public String showHome(){
-		
+
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String showHome() {
+
 		LOGGER.debug("Logging Rendering home view");
 		return "home";
 	}
 
+	
 }
